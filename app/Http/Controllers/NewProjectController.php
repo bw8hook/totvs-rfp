0d9b3bcd50e5a98f86006b70d9cd57121069195c
theandreluiz <andreluizgaspar1@gmail.com>
2024-10-28 11:33:56 -0300

    update navigation, rm unused components, update controller

--- app/Http/Controllers/NewProjectController.php
+++ app/Http/Controllers/NewProjectController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 class NewProjectController extends Controller
 {
     public function index() {
+        $userId = auth()->user()->id;
         $data = [
-
+            'userId' => $userId
         ];
 
         return view('newproject')->with($data);
--- resources/views/newproject.blade.php
+++ resources/views/newproject.blade.php
@@ -1,3 +1,13 @@
-<h1>
-    new project :)
-</h1>
+<x-app-layout>
+    <div class="flex items-center justify-center h-screen flex-col">
+        <h1>
+            <script>
+                const userId = @json($userId);
+                document.addEventListener('DOMContentLoaded', () => {
+                    console.log(userId);
+                });
+            </script>
+            <x-upload></x-upload>
+        </h1>
+    </div>
+</x-app-layout>
