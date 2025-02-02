b5ea15754bce6c612fe9eaba378db5409c3500f9
Vitor Carvalho <vcml@pm.me>
2025-02-02 12:36:25 -0300

    corrigindo link para logo; impondo https para toda a aplicacao

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Support\Facades\Route;
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+        $this->app['request']->server->set('HTTPS','on');
+        URL::forceScheme('https');
     }
 }
